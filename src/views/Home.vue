ea2efea6a87b88595665a8288b9dfa1d6afd28d2
kammachat <kammachat.k@ku.th>
2021-07-23 21:55:12 +0700

    clear page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Statement></Statement>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Statement from '@/components/Statement.vue'
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Statement
   }
 }
 </script>
